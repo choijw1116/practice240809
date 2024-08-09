684c1d09747aee8ed7566e0361ba768aa587251f
choijw1116 <choijw1116@gmail.com>
2024-08-09 15:18:42 +0900

    바꿨다

--- src/main/java/com/example/practice240809/VersionController.java
+++ src/main/java/com/example/practice240809/VersionController.java
@@ -1,2 +1,14 @@
-package com.example.practice240809;public class VersionController {
+package com.example.practice240809;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class VersionController {
+    @RequestMapping(method = RequestMethod.GET, value = "/version")
+    public ResponseEntity<String> getVersion(){
+        return ResponseEntity.ok("v2");
+    }
 }
